61a0f833e9ff0828457983e4f718877e240c5f89
TrendafilKostadinov <trendafil.kostadinov@gmail.com>
2023-11-02 14:27:02 +0200

    Initial commit

---
+++ src/main/java/bg/softuni/grassstore/GrassStoreApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.grassstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrassStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrassStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bg/softuni/grassstore/GrassStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.grassstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrassStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
