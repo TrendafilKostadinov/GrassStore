5dbbaeb64e9ccdf1516d275b504bc511ae0710a0
TrendafilKostadinov <trendafil.kostadinov@gmail.com>
2023-11-20 16:12:30 +0200

    Test for Customer service added

---
+++ src/test/java/bg/softuni/grassstore/service/CustomerServiceTest.java
@@ -0,0 +1,29 @@
+package bg.softuni.grassstore.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addCustomer() {
+    }
+
+    @Test
+    void getAllCustomersByTrader() {
+    }
+
+    @Test
+    void getCustomer() {
+    }
+
+    @Test
+    void deleteCustomer() {
+    }
+}
