a05193ed9a9123e7c74a04a51f3f65a4e23067b4
TrendafilKostadinov <trendafil.kostadinov@gmail.com>
2023-12-04 13:36:29 +0200

    User roles permissions changed

--- src/main/java/bg/softuni/grassstore/config/SecurityConfiguration.java
+++ src/main/java/bg/softuni/grassstore/config/SecurityConfiguration.java
@@ -20,28 +20,29 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(
-                        // Define which urls are visible by which users
                         authorizeRequests -> authorizeRequests
-                                // All static resources which are situated in js, images, css are available for anyone
                                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                                // Allow anyone to see the home page, the registration page and the login form
                                 .requestMatchers("/login", "/login-error", "/").permitAll()
-//                        .requestMatchers("/offers/all").permitAll()
-//                        .requestMatchers(HttpMethod.GET, "/offer/**").permitAll()
                                 .requestMatchers("/error").permitAll()
                                 .requestMatchers(
                                         "/user-add",
                                         "/admin/user-password/**",
-                                        "/user-delete/**").hasRole(RoleNames.ADMIN.name())
+                                        "/user-delete/**",
+                                        "/user-detail/**").hasRole(RoleNames.ADMIN.name())
+                                .requestMatchers(
+                                        "/delivery-add/**",
+                                        "/product-add",
+                                        "/order-ship/**").hasRole(RoleNames.OSB.name())
+                                .requestMatchers(
+                                        "/address-add",
+                                        "/customer-add",
+                                        "/order-add/**").hasRole(RoleNames.TRADER.name())
                                 .requestMatchers("/assign-role").hasRole(RoleNames.MANAGER.name())
                                 .anyRequest().authenticated()
                 ).formLogin(
                         formLogin -> {
                             formLogin
-                                    // redirect here when we access something which is not allowed.
-                                    // also this is the page where we perform login.
                                     .loginPage("/login")
-                                    // The names of the input fields (in our case in login.html)
                                     .usernameParameter("email")
                                     .passwordParameter("password")
                                     .defaultSuccessUrl("/home")
@@ -50,11 +51,8 @@ public class SecurityConfiguration {
                 ).logout(
                         logout -> {
                             logout
-                                    // the URL where we should POST something in order to perform the logout
                                     .logoutUrl("/logout")
-                                    // where to go when logged out?
                                     .logoutSuccessUrl("/login")
-                                    // invalidate the HTTP session
                                     .invalidateHttpSession(true);
                         }
                 ).sessionManagement()
